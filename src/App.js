3d9f3b599a5b523afdd7d1a5eb819a50b4b8a066
Vivek Dubey <dubey.vivek.2805@gmail.com>
2020-04-18 00:59:13 +0530

    First React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+  
+  // Create the handleClick method here
+  handleClick() {
+    this.setState({count: this.state.count + 1});
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.count}
+        </h1>
+        {/* Add an onClick event in the <button> tag */}
+        <button onClick = {() => {this.handleClick()}}>+</button>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
